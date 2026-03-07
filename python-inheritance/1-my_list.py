62798c10b168f737a276ba14c14ca887f91b6fbc
Sevinc <sevinc.abdullayeva2008@gmail.com>
2026-03-07 14:20:47 +0000

    taask

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines MyList class"""
+
+
 class MyList(list):
-    """MyList class"""
+    """MyList class that inherits from list"""
 
     def print_sorted(self):
-        """Print the list sorted"""
+        """Print the list sorted in ascending order"""
         print(sorted(self))
