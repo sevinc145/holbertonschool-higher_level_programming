3bc9c216155d90633adc330d1a84d9a18947c8db
sevinc <sevinc.abdullayeva2008@gmail.com>
2026-03-21 14:50:06 +0000

    task

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if not exists with password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Give only SELECT privilege on that database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
