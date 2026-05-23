ca7534447a0ee006ade2542bc855cfff68383841
sevinc145 <sevinc.abdullayeva2008@gmail.com>
2026-05-23 13:17:27 +0400

    Add click event to change header color to red

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,6 +1,7 @@
 const header = document.querySelector('header');
 const redHeader = document.querySelector('#red_header');
 
+
 redHeader.addEventListener('click', function() {
   header.style.color = '#FF0000';
 });
